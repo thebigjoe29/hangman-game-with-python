e67c033dc2da44212dcf0c568f2f765d8da534f5
thebigjoe29 <99957540+thebigjoe29@users.noreply.github.com>
2022-02-24 00:15:38 +0530

    Create hangmanpython.py

---
+++ hangmanpython.py
@@ -0,0 +1,28 @@
+import random
+f=random.randint(0,4)
+tp=("apple","banana","orange","pears","pomegranate")
+secw=tp[f]
+ctr=0
+n=int(input("enter no of tries: "))
+if n>2:
+    while ctr<n:
+        x=input("enter guess: ")
+        if x.isalpha():
+         if x.lower()==secw.lower():
+            print("you won!")
+            break
+         elif x!=secw:
+             for i in x:
+              if i in secw:
+                 print(i, end="")
+             ctr+=1
+         else:
+            ctr+=1
+         if ctr==2:
+            r=input("do you want a hint?: ")
+            if r=="Y":
+                print(secw[0])
+        else:
+            print("Invalid input")
+    if ctr==n:
+     print("you lost!")
